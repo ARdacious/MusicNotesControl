03bfebe33d4a25a9c3df230c569ce6e798966e4e
Arrie Dacious <ARdaciousDev@gmail.com>
2019-05-22 23:34:57 +0200

    [Improve note rendering]
    Add Type and Duration properties
    Correct line on whicht to render a note
    Vary visualisation based on properties
    
    
    Signed-off-by: Arrie Dacious <ARdaciousDev@gmail.com>

--- MusicNotesControll.Designer.cs
+++ MusicNotesControll.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MusicNotesControll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "MusicNotesControll";
+            this.MouseEnter += new System.EventHandler(this.MusicNotesControll_MouseEnter);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- MusicNotesControll.cs
+++ MusicNotesControll.cs
@@ -20,24 +20,24 @@ namespace MusicNotes
     {
         public delegate void DrawMusicNotes(Graphics g);
 
-        List<Note> notes = new List<Note>();
-
-        string unicodestuff = "\u266d \u266f \u266e"; // bes sharp normal
+        public List<Note> Notes = new List<Note>();
 
         public MusicNotesControll()
         {
             InitializeComponent();
-            notes.Add(new Note('A', 4, 0, false));
-            notes.Add(new Note('B', 4, 1, true));
-            notes.Add(new Note('E', 4, -1, true));
-            notes.Add(new Note('A', 4, 0, true));
-            notes.Add(new Note('A', 4, 0, false));
-            notes.Add(new Note('G', 4, 0, false));
-            notes.Add(new Note('A', 5, 0, false));
-            notes.Add(new Note('D', 3, 0, false));
-            notes.Add(new Note('E', 3, 0, false));
-            notes.Add(new Note('F', 5, 0, false));
-            notes.Add(new Note('A', 4, 0, false));
+            maxWidth = Width;
+            Notes.Add(new Note('A', 4, 0, false, "whole", 16));
+            Notes.Add(new Note('G', 3, 1, true, "quarter", 8));
+            Notes.Add(new Note('A', 3, -1, true, "eighth", 4));
+            Notes.Add(new Note('B', 3, 0, true, "16th", 2));
+            Notes.Add(new Note('C', 4, 0, false, "32th", 1));
+            Notes.Add(new Note('D', 4, 0, false, "whole", 16));
+            Notes.Add(new Note('E', 4, 0, false, "whole", 16));
+            Notes.Add(new Note('F', 4, 0, false, "whole", 16));
+            Notes.Add(new Note('G', 4, 0, false, "whole", 16));
+            Notes.Add(new Note('A', 4, 0, false, "whole", 16));
+            Notes.Add(new Note('B', 4, 0, false, "whole", 16));
+            Notes.Add(new Note('C', 5, 0, false, "whole", 16));
 
         }
         static int fontSize = 16;
@@ -61,6 +61,8 @@ namespace MusicNotes
             g.TranslateTransform(10, 45);
             // draw some lines
             DrawLines(g, 5);
+            // handle scroll
+            g.TranslateTransform(-(scrollValue * maxWidth / 100), 0);
             // draw content
             drawDefaultNotes(g);
             
@@ -69,25 +71,27 @@ namespace MusicNotes
         {
             // draw some notes
             int cnt = 0;
-            foreach (Note note in notes)
+            foreach (Note note in Notes)
             {
                 cnt += 1;
                 //Console.WriteLine("{0}: {1} {2}", on, l, on.NoteLength);
-                DrawNote(g, noteBrush, note.Step, note.Octave, note.Alter, note.ShowAlter, cnt%8, cnt*6);
+                DrawNote(g, noteBrush, note.Step, note.Octave, note.Alter, note.ShowAlter, note.Type, cnt*6);
             }
 
         }
         int LineHeight = 5;
         int NoteSpacing = 8;
-        string[] alterLookUp = new string[] { "\u266d", "\u266e", "\u266f" };
-        protected void DrawNote(Graphics g, Brush brush, char note, int octave, int alter, bool showalter, int length, int time)
+        
+        protected void DrawNote(Graphics g, Brush brush, char step, int octave, int alter, bool showalter, string type, int time)
         {
             // base A4 midi note
-            var noteline = 'A' - note + 4 ;
-            noteline += (octave - 4) * -8; // do not count the black keys
-            Console.WriteLine("linenr {0}", noteline);
+            var noteline = (step - 'A' + 5) % 7;
+            noteline += (octave - 4) * 7; // do not count the black keys
+            noteline *= -1; // invert
+            noteline += 9;
+            //Console.WriteLine("linenr {0}", noteline);
             // first three notes do not have a top bar
-            int numBars = length - 3;
+            int numBars = 0;
             // longer note tail for more bars
             int tailLenght = numBars > 2 ? 30 : 25;
             var s = g.Save();
@@ -95,29 +99,31 @@ namespace MusicNotes
             g.TranslateTransform(time * NoteSpacing, 0);
             if (noteline < -2)
             {
-                // odd lines need to draw when not on the available 5 lines drawn
-                for (int i = -3; i >= noteline; i-=2)
-                    g.DrawLine(barlinePen, -5, (i * LineHeight), dim.X+5, i*LineHeight);
+                // even lines need to draw when not on the available 5 lines drawn
+                for (int i = -2; i > noteline; i-=2)
+                    g.DrawLine(barlinePen, -5, i*LineHeight, dim.X+5, i*LineHeight);
             }
             if (noteline > 8)
             {
-
+                // even lines need to draw when not on the available 5 lines drawn
+                for (int i = 8; i <= noteline+1; i += 2)
+                    g.DrawLine(barlinePen, -5, i*LineHeight, dim.X + 5, i*LineHeight);
             }
             // translate to note position in Y
             g.TranslateTransform(0, (noteline * LineHeight) + 2);
             // draw alter (5 and up is special for not drawing)
             if (showalter)
             {
-                var m = g.MeasureString(alterLookUp[1 + alter], drawFont);
+                var m = g.MeasureString(Note.alterLookUp[1 + alter], drawFont);
                 //Console.WriteLine("measured {0} for {1}", m, alter);
-                g.DrawString(alterLookUp[1 + alter], drawFont, drawBrush, -(Math.Min(22, m.Width) - 2), -fontSize / 2, drawFormat);
+                g.DrawString(Note.alterLookUp[1 + alter], drawFont, drawBrush, -(Math.Min(22, m.Width) - 2), -fontSize / 2, drawFormat);
             }
             g.RotateTransform(-15f);
             // draw note elipse (fill or open)
-            switch(length)
+            switch(type)
             {
-                case 1:
-                case 2:
+                case "whole":
+                case "half":
                     g.DrawEllipse(notePen, 1, 1, dim.X-2, dim.Y-2);
                     break;
                 default:
@@ -125,13 +131,28 @@ namespace MusicNotes
                     break;
             }
             // draw line from elipse (for all but full length note)
-            switch (length)
+            switch (type)
             {
-                case 1:
+                case "whole":
                     break;
                 default:
                     g.RotateTransform(+30f);
                     g.DrawLine(notePen, dim.X, 0, dim.X, -tailLenght);
+                    break;
+            }
+            switch (type)
+            {
+                case "eighth":
+                    numBars = 1;
+                    break;
+                case "16th":
+                    numBars = 2;
+                    break;
+                case "32th":
+                    numBars = 3;
+                    break;
+                default:
+                    numBars = 0;
                     break;
             }
             var pts = new List<Point>();
@@ -151,7 +172,8 @@ namespace MusicNotes
             {
                 g.DrawLine(noteBarPen, p, new Point(p.X + 10, p.Y));
             }
-
+            // determine max widht
+            maxWidth = Math.Max(maxWidth, time * NoteSpacing);
         }
         protected void DrawLines(Graphics g, int count)
         {
@@ -175,6 +197,23 @@ namespace MusicNotes
             }
         }
         private string demoStringValue = null;
+        private int scrollValue = 0;
+        private int maxWidth = 0;
+
+        public int ScrollValue
+        {
+            get
+            {
+                return scrollValue;
+            }
+            set
+            {
+                scrollValue = value;
+                Invalidate();
+            }
+        }
+
+
         [Browsable(true)]
         public string DemoString
         {
@@ -187,23 +226,46 @@ namespace MusicNotes
                 demoStringValue = value;
             }
         }
+
+        private void MusicNotesControll_MouseEnter(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
     }
+
     public class Note
     {
-        //                                    A,  B, C  D  E  F  G 
-        static int[] MidiLookup = new int[] { 9, 11, 0, 2, 4, 5, 7 }; // from C = 0
-        public Note(char step, int octave, int alter, bool show)
+        public static string[] alterLookUp = new string[] { "\u266d", "\u266e", "\u266f" };
+        public static string[] alterLookUpASCII = new string[] { "b", "o", "#" };
+        //                                    C  D  E  F  G  A  B
+        static int[] MidiLookup = new int[] { 0, 2, 4, 5, 7, 9, 11 }; // from C = 0
+        public Note(char step, int octave, int alter, bool show, string type, int duration)
         {
             Step = step;
             Octave = octave;
             Alter = alter;
             ShowAlter = show;
-            Midi = (octave * 12) + MidiLookup[step - 'A'] + alter;
+            var idx = (step - 'A' + 5) % 7;
+            Midi = (octave * 12) + MidiLookup[idx] + alter + 12;
+            Type = type;
+            Duration = duration;
+        }
+        public override string ToString() {
+            if (ShowAlter)
+            {
+                return string.Format("Note: {2}{0}{1} ({3})", Step, Octave, alterLookUpASCII[Alter + 1], Midi);
+            }
+            else
+            {
+                return string.Format("Note: {0}{1} ({3})", Step, Octave, alterLookUpASCII[Alter + 1], Midi);
+            }
         }
         public char Step;
         public int Octave;
         public int Alter;
         public bool ShowAlter;
+        public string Type;
         public int Midi;
+        public int Duration;
     }
 }
